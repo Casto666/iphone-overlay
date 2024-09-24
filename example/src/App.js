fe5da9cec31e2ca03b5c973567bd223f249db2fe
digity <digity@git.roflo.lol>
2024-09-24 11:33:21 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'iphone-overlay'
+import 'iphone-overlay/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
